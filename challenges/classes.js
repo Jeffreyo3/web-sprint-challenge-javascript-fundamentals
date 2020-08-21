a3f823eb03d06df9fa64b2403b67ef2dcc18dd9c
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-08-21 09:48:12 -0700

    Update CubeMaker class to take in a single parameter

--- challenges/classes.js
+++ challenges/classes.js
@@ -32,11 +32,7 @@ console.log(cuboid.surfaceArea()); // 130
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 class CubeMaker extends ClassCuboidMaker {
   constructor(attrs) {
-    super({
-      length: attrs.side,
-      width: attrs.side,
-      height: attrs.side,
-    });
+    super(attrs);
   }
   volume() {
     return Math.pow(this.length, 3);
@@ -46,7 +42,7 @@ class CubeMaker extends ClassCuboidMaker {
   }
 }
 
-const cube = new CubeMaker({ side: 5 });
+const cube = new CubeMaker({ length: 5 });
 console.log(cube);
 console.log(cube.volume) // should be f return Math.pow(this.length, 3);
 console.log(cube.surfaceArea) // should be f return 6 * Math.pow(this.length, 2);
